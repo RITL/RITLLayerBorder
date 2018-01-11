48bf8386cb29604a9381b4f4c1ea6582bd0d61c3
774655048@qq.com <774655048@qq.com>
2018-01-11 11:23:06 +0800

    Initial Commit

---
+++ RITLBorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RITLBorder
+//
+//  Created by YueWen on 2018/1/11.
+//  Copyright © 2018年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RITLBorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RITLBorder
+//
+//  Created by YueWen on 2018/1/11.
+//  Copyright © 2018年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
